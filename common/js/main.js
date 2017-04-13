a8bc0ca28af76e28256cf150fc66c41462adf0a1
Dim And <dimand81179@gmail.com>
2017-04-13 18:53:28 +0300

    Top page

--- common/js/main.js
+++ common/js/main.js
@@ -3,7 +3,7 @@ function initPage(){
 	swiperGallery1();
 	swiperGallery2();
 	mobileMenu();
-	fixedNav();
+
 }
 var allGallery = {}
 function swiperGallery1(){
@@ -22,49 +22,14 @@ function swiperGallery2(){
 		slidesPerView: 5,
 		spaceBetween: 0,
 		breakpoints: {
-			// when window width is <= 320px
+			1000: {
+				slidesPerView: 4
+			},
 			736: {
 				slidesPerView: 2
 			}
 		}
 	});
-}
-
-
-
-
-function isMobile(){
-	
-	return (window.innerWidth <= 736 )? true : false;
-	
-}
-function fixedNav(){
-	
-	if(!$('body').hasClass('index') || isMobile())return;
-	
-	
-	headMenu = document.querySelector('.nav'),  
-	needToScroll = headMenu.offsetTop;
-	
-	$(window).scroll(function(e){
-		
-		console.log(isMobile());
-		if(isMobile()){
-			$('body').removeClass('fixed-nav');
-			return;
-		}
-		
-		var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-		
-		if(scrollPosition >= needToScroll){
-			$('body').addClass('fixed-nav');
-		}
-		else{
-			$('body').removeClass('fixed-nav');
-		}
-		
-	});
-	
 }
 
 /* Mobile Menu */
@@ -76,7 +41,7 @@ function mobileMenu(){
 	});
 	
 	if ($(window).width() <= 736) {
-		$('.nav a').click(function(e) {
+		$('.nav ul a').click(function(e) {
 			e.preventDefault();
 			$('body').removeClass('nav-visible');
 			$('.nav').slideUp(300);
